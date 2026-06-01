2005474794bd1a12874c8e6fa6e6fb83b465e20a
qhrudlim <qhrudlim@gmail.com>
2026-06-01 14:18:21 +0900

    update portfolio
    delete phone number

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,10 +8,6 @@ const Contact = () => {
         <p className="contact-intro">제 포트폴리오를 읽어주셔서 감사합니다. <br/>편하게 연락주세요!</p>
         <div className="contact-details">
           <div className="contact-item">
-            <strong>Phone</strong>
-            <span>010-3396-6856</span>
-          </div>
-          <div className="contact-item">
             <strong>Email</strong>
             <span>qhrudlim@gmail.com</span>
           </div>

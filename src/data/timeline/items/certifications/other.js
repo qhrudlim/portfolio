9b8cb94a6eaf6a4545989ea83fef703af50974d5
qhrudlim <qhrudlim@gmail.com>
2026-06-19 15:47:52 +0900

    update portfolio
    add car certification

--- src/data/timeline/items/certifications/other.js
+++ src/data/timeline/items/certifications/other.js
@@ -7,6 +7,13 @@ export const otherCertificationTimeline = [
     category: 'Certification'
   },
   {
+    date: '2017.12.04',
+    sortDate: '2017-12-04',
+    title: '자동차운전면허(1종 보통) 취득',
+    description: '경찰청 발행',
+    category: 'Certification'
+  },
+  {
     date: '2016.02.11',
     sortDate: '2016-02-11',
     title: '한국사능력검정시험 2급 취득',
